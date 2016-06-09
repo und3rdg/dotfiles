f28b9a3fd1531fc8002cbdb6e41abab6b68c4058
und3rdg <und3rdg@gmail.com>
2016-06-09 23:04:19 +0100

    install.sh fix

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# file_list=( '.config/' '.myconf/' '.vim/' '.bashrc' '.bash_profile' '.gitconfig' '.tmux.conf' '.xbindkeysr' '.xinitrc' '.zprofile' '.zshrc' )
+file_list=( ".config/" ".myconf/" ".vim/" ".bashrc" ".bash_profile" ".gitconfig" ".tmux.conf" ".xbindkeysr" ".xinitrc" ".zprofile" ".zshrc" )
 
-trash='~/tmp/dotfilesOld/'
-home_dir='~/'
-dot_dir='~/.dotfiles/'
+trash="~/tmp/dotfilesOld/"
+home_dir="~/"
+dot_dir="~/.dotfiles/"
 
 if [ ! -f $trash ]
 then
-    echo 'mkdir -pv' $trash
+    echo "mkdir -pv" $trash
     mkdir -pv $trash
 fi
 
@@ -16,7 +16,7 @@ for file in ${file_list[@]}
 do
     if [ -f $home_dir$file ]
     then
-        echo 'mv -iv' $home_dir$file $trash$file
+        echo "mv -iv" $home_dir$file $trash$file
         mv -iv $home_dir$file $trash$file
     fi
     ln -siv $dot_dir$file $home_dir$file
