0a36e89adea23ad625fb87e12e4e5fee61bf708a
und3rdg <und3rdg@gmail.com>
2016-06-09 16:35:14 +0100

    install.sh v1

--- install.sh
+++ install.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
-fileList=(".bashrc" ".myconf/" ".tmux.conf" ".vim/" ".xinitrc" ".zshrc")
-trash="tmp/dotfilesOld"
+file_list=('.bashrc' '.myconf/' '.tmux.conf' '.vim/' '.xinitrc' '.zshrc')
+trash='~/tmp/dotfilesOld/'
+home_dir='~/'
+dot_dir='~/.dotfiles/'
 
-if [ ! -f "$trash" ]
-   mkdir -p "$trash"
+if [ ! -f $trash ]
+then
+    echo 'mkdir -pv' $trash
+    mkdir -pv $trash
 fi
 
-for file in "${fileList[@]}
+for file in ${file_list[@]}
 do
-    if [ -f "~/$file" ]
-        mv "~/$file ~/$trash/$file"
+    if [ -f $home_dir$file ]
+    then
+        echo 'mv -iv' $home_dir$file $trash$file
+        mv -iv $home_dir$file $trash$file
     fi
-    ln -s "~/dotfiles/$file ~/$file"
+    ln -siv $dot_dir$file $home_dir$file
 done
