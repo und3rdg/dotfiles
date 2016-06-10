b184555bbb9242425ec434996da7eda64babb2da
und3rdg <und3rdg@gmail.com>
2016-06-10 05:38:44 +0100

    install auto list all files from folder, exclude file list, organize code

--- install.sh
+++ install.sh
@@ -1,23 +1,77 @@
 #!/bin/sh
+###############################################
+# Lets automate symlinkings to dotfiles.
+# Old configs if exists will move to 
+# .trash/oldDotfiles
+# install.sh have to by run in directory 
+# with dotfiles
+###############################################
 
-file_list=( ".config/" ".myconf/" ".vim/" ".bashrc" ".bash_profile" ".gitconfig" ".tmux.conf" ".xbindkeysr" ".xinitrc" ".zprofile" ".zshrc" )
 
-trash="~/tmp/dotfilesOld/"
-home_dir="$HOME/"
-dot_dir="$HOME/.dotfiles/"
+# LOCATION OF DOTFILES 
+dot_dir=$HOME/.dotfiles
 
-if [ ! -f $trash ]
-then
-    echo "mkdir -pv" $trash
-    mkdir -pv $trash
-fi
+# PLACE TO PUT OLD DOTFILES
+trash=$HOME/.trash/oldDotfiles
 
-for file in ${file_list[@]}
-do
-    if [ -f $home_dir$file ]
+
+# ALL FILES FROM CURENT DIR 
+file_list=(* .[^.]*)
+
+# EXCLUDE FILE(S) THAT ARE NOT CONFIGS
+exclude_list=("install.sh" "README.md" ".git" ".gitignore")
+
+function filterFiles() {
+    for file_exclude in "${exclude_list[@]}"
+    do
+        tmp_array=()
+        for file_check in "${file_list[@]}"
+        do
+            if [ $file_check != $file_exclude ]
+            then
+                tmp_array+=($file_check)
+            fi
+        done
+        file_list=("${tmp_array[@]}")
+        unset tmp_array
+    done
+}
+
+
+function createTrash() {
+    if [ ! -f "$trash" ]
     then
-        echo "mv -iv" $home_dir$file $trash$file
-        mv -iv $home_dir$file $trash$file
+        echo mkdir -pv $trash
+        mkdir -pv "$trash"
     fi
-    ln -siv $dot_dir$file $home_dir$file
-done
+}
+
+
+function trashOldDots() {
+    if [ -f "$HOME/$1" ]
+    then
+        echo mv -iv $HOME/$1 $trash/$1
+        mv -iv "$HOME/$1" "$trash/$1"
+    fi
+}
+
+function symlinkDots() {
+    for file in ${file_list[@]}
+    do
+        trashOldDots $file
+        echo ln -siv $dot_dir/$file $HOME/$file
+        ln -siv "$dot_dir/$file" "$HOME/$file"
+    done
+}
+
+
+filterFiles
+createTrash
+symlinkDots
+
+
+unset -f symlinkDots
+unset -f filterFiles
+unset -f trashOldDots
+
+
